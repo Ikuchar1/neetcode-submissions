2371f8f4ea9010e57ab1c0d62f7043a8e6c8ef63
Ian Kuchar <ian1kuchar@gmail.com>
2026-07-31 13:24:31 -0500

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int MaxDepth(TreeNode root) {
+
+        //go and rescurse over left child and right child. 
+        
+        //check if node is null
+        if(root == null){
+            return 0;
+        } else {
+            return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+        }
+
+    }
+}
