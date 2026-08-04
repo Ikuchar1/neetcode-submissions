38ed302cd7d098919b2ec6f5f55180268cae5de9
Ian Kuchar <ian1kuchar@gmail.com>
2026-08-04 10:57:52 -0500

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cs
@@ -0,0 +1,37 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool CanAttendMeetings(List<Interval> intervals) {
+        //go and sort by starting time
+        //then go one by one, if the next start time is less than the current end time return false
+
+        var sorted = intervals.OrderBy(p => p.start).ToList();
+
+        // foreach(Interval interv in sorted){
+        //     Console.WriteLine(interv.start);
+        //     Console.WriteLine(interv.end);
+        // }
+
+        for(int i = 0; i < sorted.Count - 1; i++){
+            Interval interv1 = sorted[i];
+            Interval interv2 = sorted[i + 1];
+            Console.WriteLine(interv1.start);
+            Console.WriteLine(interv1.end);
+
+            if(interv2.start < interv1.end){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
